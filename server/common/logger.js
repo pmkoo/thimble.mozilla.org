c68e3fddc24206fcc32deb5af99e42f95beba86a
赵少辉 <zhaoshaohui@pmkoo.com>
2017-01-16 20:01:29 +0800

    adf

--- server/common/logger.js
+++ server/common/logger.js
@@ -5,7 +5,7 @@ var log4js = require('log4js');
 log4js.configure({
   appenders: [
     { type: 'console' },
-    { type: 'file', filename: 'logs/cheese.log', category: 'cheese' }
+    { type: 'file', filename: __dirname+'/../../logs/cheese.log', category: 'cheese' }
   ]
 });
 
